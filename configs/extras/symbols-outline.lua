9ae4fdd2e3c51d25502148e1d40b8b23bcd07150
Daniel Boll <43689101+Daniel-Boll@users.noreply.github.com>
2023-03-31 15:58:13 +0000

    feat(extras): add `symbols-outline.lua`

---
+++ configs/extras/symbols-outline.lua
@@ -0,0 +1,14 @@
+local spec = {
+  "simrat39/symbols-outline.nvim",
+  -- Default keys, may be overridden by user.
+  keys = {
+    {
+      "<leader>cs",
+      "<cmd>SymbolsOutline<cr>",
+      desc = "Symbols Outline",
+    },
+  },
+  config = true,
+}
+
+return spec
