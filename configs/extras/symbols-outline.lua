db4716a91f180e30ca54ec742b58715b69d4d61f
Daniel Boll <danielboll.academico@gmail.com>
2023-04-02 12:06:39 -0300

    fix(extras): lazy load symbols-outline by cmd
    Signed-off-by: Daniel Boll <danielboll.academico@gmail.com>

--- configs/extras/symbols-outline.lua
+++ configs/extras/symbols-outline.lua
@@ -1,13 +1,6 @@
 local spec = {
   "simrat39/symbols-outline.nvim",
-  -- Default keys, may be overridden by user.
-  keys = {
-    {
-      "<leader>cs",
-      "<cmd>SymbolsOutline<cr>",
-      desc = "Symbols Outline",
-    },
-  },
+  cmd = "SymbolsOutline",
   config = true,
 }
 
